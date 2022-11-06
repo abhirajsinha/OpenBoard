62d0bfe5070b410ab5bb8126df1c5ee520b386f4
Abhiraj <abhirajsinha25@gmail.com>
2022-11-06 21:57:03 +0530

    added drag and drop feature

--- tools.js
+++ tools.js
@@ -4,6 +4,7 @@ let pencilToolCont = document.querySelector(".pencil-tool-cont");
 let eraserToolCont = document.querySelector(".eraser-tool-cont");
 let pencil = document.querySelector(".pencil");
 let eraser = document.querySelector(".eraser");
+let sticky = document.querySelector(".sticky");
 let pencilFlag = false;
 let eraserFlag = false;
 let optionsFlag = true;
@@ -33,7 +34,6 @@ function closeTools() {
   toolsCont.style.display = "none";
   pencilToolCont.style.display = "none";
   eraserToolCont.style.display = "none";
-  
 }
 
 pencil.addEventListener("click", (e) => {
@@ -57,3 +57,58 @@ eraser.addEventListener("click", (e) => {
     eraserToolCont.style.display = "none";
   }
 });
+
+sticky.addEventListener("click", (e) => {
+  let stickyCont = document.createElement("div");
+  stickyCont.setAttribute("class", "sticky-cont");
+  stickyCont.innerHTML = `   
+    <div class="header-cont">
+    <div class="minimize"></div>
+    <div class="remove"></div>
+  </div>
+  <div class="note-cont">
+    <textarea ></textarea>
+  </div> 
+    `;
+
+  document.body.append(stickyCont);
+  //Drag and drop feature
+  stickyCont.onmousedown = function (event) {
+    dragAndDrop(stickyCont, event);
+  };
+
+  stickyCont.ondragstart = function () {
+    return false;
+  };
+});
+
+function dragAndDrop(stickyCont, event) {
+  let shiftX = event.clientX - stickyCont.getBoundingClientRect().left;
+  let shiftY = event.clientY - stickyCont.getBoundingClientRect().top;
+
+  stickyCont.style.position = "absolute";
+  stickyCont.style.zIndex = 1000;
+  document.body.append(stickyCont);
+
+  moveAt(event.pageX, event.pageY);
+
+  // moves the stickyCont at (pageX, pageY) coordinates
+  // taking initial shifts into account
+  function moveAt(pageX, pageY) {
+    stickyCont.style.left = pageX - shiftX + "px";
+    stickyCont.style.top = pageY - shiftY + "px";
+  }
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  // move the stickyCont on mousemove
+  document.addEventListener("mousemove", onMouseMove);
+
+  // drop the stickyCont, remove unneeded handlers
+  stickyCont.onmouseup = function () {
+    document.removeEventListener("mousemove", onMouseMove);
+    stickyCont.onmouseup = null;
+  };
+}
