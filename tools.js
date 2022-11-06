307c35e184b4dced86c6cb4a907d705f9f8c1bd5
Abhiraj <abhirajsinha25@gmail.com>
2022-11-06 21:24:57 +0530

    added ui features

--- tools.js
+++ tools.js
@@ -1,10 +1,17 @@
 let optionsCont = document.querySelector(".options-cont");
+let toolsCont = document.querySelector(".tools-cont");
+let pencilToolCont = document.querySelector(".pencil-tool-cont");
+let eraserToolCont = document.querySelector(".eraser-tool-cont");
+let pencil = document.querySelector(".pencil");
+let eraser = document.querySelector(".eraser");
+let pencilFlag = false;
+let eraserFlag = false;
 let optionsFlag = true;
 
 optionsCont.addEventListener("click", (e) => {
   //Modify UI
   optionsFlag = !optionsFlag;
-  
+
   if (optionsFlag) {
     openTools();
   } else {
@@ -13,13 +20,40 @@ optionsCont.addEventListener("click", (e) => {
 });
 
 function openTools() {
-    let iconElem = optionsCont.children[0];
-    iconElem.classList.remove("fa-times")
-    iconElem.classList.add("fa-bars")
+  let iconElem = optionsCont.children[0];
+  iconElem.classList.remove("fa-times");
+  iconElem.classList.add("fa-bars");
+  toolsCont.style.display = "flex";
 }
 
 function closeTools() {
-    let iconElem = optionsCont.children[0];
-    iconElem.classList.remove("fa-bars")
-    iconElem.classList.add("fa-times")
+  let iconElem = optionsCont.children[0];
+  iconElem.classList.remove("fa-bars");
+  iconElem.classList.add("fa-times");
+  toolsCont.style.display = "none";
+  pencilToolCont.style.display = "none";
+  eraserToolCont.style.display = "none";
+  
 }
+
+pencil.addEventListener("click", (e) => {
+  pencilFlag = !pencilFlag;
+  //true - show pencil tools
+  //false - hide
+
+  if (pencilFlag) {
+    pencilToolCont.style.display = "block";
+  } else {
+    pencilToolCont.style.display = "none";
+  }
+});
+
+eraser.addEventListener("click", (e) => {
+  eraserFlag = !eraserFlag;
+
+  if (eraserFlag) {
+    eraserToolCont.style.display = "flex";
+  } else {
+    eraserToolCont.style.display = "none";
+  }
+});
